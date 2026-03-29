e2bb80461dcc522b54ad919bbaf842ba3f576ecd
OUMDEV <aum0834@gmail.com>
2026-03-29 23:32:48 +0700

    feat(ui): enhance login and authentication pages with styling updates and new grain effect

--- apps/web-admin/app/login/page.tsx
+++ apps/web-admin/app/login/page.tsx
@@ -1,8 +1,8 @@
-import Link from "next/link";
+import Link from "next/link"
 
-import { cn } from "@workspace/ui/lib/utils";
-import { buttonVariants } from "@workspace/ui/components/button";
-import { LoginForm } from "./login-form";
+import { cn } from "@workspace/ui/lib/utils"
+import { buttonVariants } from "@workspace/ui/components/button"
+import { LoginForm } from "./login-form"
 
 export default function LoginPage() {
   return (
@@ -12,16 +12,17 @@ export default function LoginPage() {
         className={cn(
           buttonVariants({ variant: "ghost" }),
           "absolute top-4 right-4 md:top-8 md:right-8"
-        )}>
+        )}
+      >
         Sign Up
       </Link>
-      <div className="text-primary relative hidden h-full flex-col p-10 lg:flex dark:border-r">
+      <div className="relative hidden h-full flex-col p-10 text-primary lg:flex dark:border-r">
         <div
-          className="bg-primary/5 absolute inset-0"
+          className="grain absolute inset-0 rounded-xl bg-primary/5 p-2"
           style={{
-            backgroundImage: "url(/assets/images/bg/bg02.jpg)",
+            backgroundImage: "url(/assets/images/bg/bg01.jpg)",
             backgroundSize: "cover",
-            backgroundPosition: "center top",
+            backgroundPosition: "center bottom",
           }}
         />
         <div className="relative z-20 flex items-center text-lg font-medium text-white/80">
@@ -33,7 +34,8 @@ export default function LoginPage() {
             strokeWidth="2"
             strokeLinecap="round"
             strokeLinejoin="round"
-            className="mr-2 h-6 w-6">
+            className="mr-2 h-6 w-6"
+          >
             <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
           </svg>
           Acme Inc
@@ -52,21 +54,22 @@ export default function LoginPage() {
             <h1 className="text-2xl font-semibold tracking-tight">
               Welcome back
             </h1>
-            <p className="text-muted-foreground text-sm">
+            <p className="text-sm text-muted-foreground">
               Enter your credentials to sign in to your account
             </p>
           </div>
           <LoginForm />
-          <p className="text-muted-foreground px-8 text-center text-sm">
+          <p className="px-8 text-center text-sm text-muted-foreground">
             Don&apos;t have an account?{" "}
             <Link
               href="/"
-              className="hover:text-primary underline underline-offset-4">
+              className="underline underline-offset-4 hover:text-primary"
+            >
               Sign up
             </Link>
           </p>
         </div>
       </div>
     </div>
-  );
+  )
 }
--- packages/ui/src/components/authentication/page.tsx
+++ packages/ui/src/components/authentication/page.tsx
@@ -1,8 +1,8 @@
-import Link from "next/link";
+import Link from "next/link"
 
-import { cn } from "@workspace/ui/lib/utils";
-import { buttonVariants } from "@workspace/ui/components/button";
-import { UserAuthForm } from "./components/user-auth-form";
+import { cn } from "@workspace/ui/lib/utils"
+import { buttonVariants } from "@workspace/ui/components/button"
+import { UserAuthForm } from "./components/user-auth-form"
 
 export default function Page() {
   return (
@@ -12,16 +12,17 @@ export default function Page() {
         className={cn(
           buttonVariants({ variant: "ghost" }),
           "absolute top-4 right-4 md:top-8 md:right-8"
-        )}>
+        )}
+      >
         Login
       </Link>
-      <div className="text-primary relative hidden h-full flex-col p-10 lg:flex dark:border-r">
+      <div className="relative hidden h-full flex-col p-10 text-primary lg:flex dark:border-r">
         <div
-          className="bg-primary/5 absolute inset-0"
+          className="grain absolute inset-0 rounded-xl bg-primary/5 p-2"
           style={{
             backgroundImage: "url(/assets/images/bg/bg01.jpg)",
             backgroundSize: "cover",
-            backgroundPosition: "center"
+            backgroundPosition: "center bottom",
           }}
         />
         <div className="relative z-20 flex items-center text-lg font-medium text-white/80">
@@ -33,34 +34,44 @@ export default function Page() {
             strokeWidth="2"
             strokeLinecap="round"
             strokeLinejoin="round"
-            className="mr-2 h-6 w-6">
+            className="mr-2 h-6 w-6"
+          >
             <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
           </svg>
           Acme Inc
         </div>
         <div className="relative z-20 mt-auto max-w-3xl text-white/70">
           <blockquote className="leading-normal text-balance">
-            &ldquo;This library has saved me countless hours of work and helped me deliver stunning
-            designs to my clients faster than ever before.&rdquo; - Sofia Davis
+            &ldquo;This library has saved me countless hours of work and helped
+            me deliver stunning designs to my clients faster than ever
+            before.&rdquo; - Sofia Davis
           </blockquote>
         </div>
       </div>
       <div className="flex items-center justify-center lg:h-screen lg:p-8">
         <div className="mx-auto flex w-full flex-col justify-center gap-6 sm:w-[350px]">
           <div className="flex flex-col gap-2 text-center">
-            <h1 className="text-2xl font-semibold tracking-tight">Create an account</h1>
-            <p className="text-muted-foreground text-sm">
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Create an account
+            </h1>
+            <p className="text-sm text-muted-foreground">
               Enter your email below to create your account
             </p>
           </div>
           <UserAuthForm />
-          <p className="text-muted-foreground px-8 text-center text-sm">
+          <p className="px-8 text-center text-sm text-muted-foreground">
             By clicking continue, you agree to our{" "}
-            <Link href="/terms" className="hover:text-primary underline underline-offset-4">
+            <Link
+              href="/terms"
+              className="underline underline-offset-4 hover:text-primary"
+            >
               Terms of Service
             </Link>{" "}
             and{" "}
-            <Link href="/privacy" className="hover:text-primary underline underline-offset-4">
+            <Link
+              href="/privacy"
+              className="underline underline-offset-4 hover:text-primary"
+            >
               Privacy Policy
             </Link>
             .
@@ -68,5 +79,5 @@ export default function Page() {
         </div>
       </div>
     </div>
-  );
+  )
 }
